27e58433b00612795fca0e5e1f6dfc0878a51c85
Tx Sun <kakinuma4ko@gmail.com>
2018-07-04 23:38:07 +0800

    Update 1_TwoSum.py

--- leetcode/1_TwoSum.py
+++ leetcode/1_TwoSum.py
@@ -1,16 +1,31 @@
-def twoSum(nums, target):# cost 600ms
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype : List[int]
-    """
-    result = []
-    for i in range(0, len(nums)):
-        for j in range(i+1, len(nums)):
-            if nums[i] + nums[j] == target:
-                result.append(i)
-                result.append(j)
-                return result
+# def twoSum(nums, target):# cost 600ms
+#     """
+#     :type nums: List[int]
+#     :type target: int
+#     :rtype : List[int]
+#     """
+#     result = []
+#     for i in range(0, len(nums)):
+#         for j in range(i+1, len(nums)):
+#             if nums[i] + nums[j] == target:
+#                 result.append(i)
+#                 result.append(j)
+#                 return result
 
 
-print twoSum([1, 2, 3, 4], 7)
+# print twoSum([1, 2, 3, 4], 7)
+
+class Solution(object):
+    def twoSum(self, nums, target):# cost 600ms
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype : List[int]
+        """
+        result = []
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    result.append(i)
+                    result.append(j)
+                    return result
